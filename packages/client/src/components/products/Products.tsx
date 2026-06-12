f09cc08d2a98674b277b1ec6949b9f573f2d5151
annekurian <anne.kurian14@gmail.com>
2026-06-12 08:40:29 +0530

    Updated Products page to display Product card

---
+++ packages/client/src/components/products/Products.tsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import { productsAPI, type Product, type ProductResponse } from "./productsAPI";
+import ProductCard from "./ProductCard";
+
+type Props = {
+  limit?: number;
+};
+
+const Products = ({ limit = 10 }: Props) => {
+  const productsQuery = useQuery<ProductResponse>({
+    queryKey: ["products", limit],
+    queryFn: () => productsAPI.fetchProducts(limit),
+  });
+
+  if (productsQuery.isError) {
+    return <p className="text-red-500">Could not fetch products. Try again!</p>;
+  }
+
+  if (!productsQuery.data?.products.length) {
+    return <p className="text-gray-500">No products available. Try again!</p>;
+  }
+  const { products } = productsQuery.data;
+  // console.log(`Products Query: ${products[0].rating}`);
+  return (
+    <div>
+      <div className="mb-5">
+        <div className="font-bold text-2xl mb-3">Products</div>
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {products.map((product: Product) => (
+            <Link to={`/products/${product.id}`}>
+              <ProductCard
+                key={product.id}
+                name={product.name}
+                brand={product.brand}
+                price={product.price}
+                rating={product.rating}
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
