e79e0d399042f29d97261c3cfb239ab68a472068
annekurian <anne.kurian14@gmail.com>
2026-06-12 08:39:52 +0530

    Added productsAPI for handling API requests related to product details

---
+++ packages/client/src/components/products/productsAPI.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export type Product = {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  rating: number;
+};
+
+export type ProductResponse = {
+  products: Product[];
+};
+
+export const productsAPI = {
+  async fetchProduct(productId: number) {
+    return axios
+      .get<Product>(`/api/products/${productId}`)
+      .then((res) => res.data);
+  },
+  async fetchProducts(limit: number) {
+    return axios
+      .get<ProductResponse>("/api/products", { params: { limit } })
+      .then((res) => res.data);
+  },
+};
