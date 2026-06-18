91c3c06de2e59cca7be4681450dc245d06f121f5
annekurian <anne.kurian14@gmail.com>
2026-06-18 19:04:27 +0530

    Updated Routes to include only Products

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,17 @@
 import { Route, Routes } from 'react-router';
-import NavBar from './components/NavBar';
-import ChatBot from './components/chat/ChatBot';
-import Products from './components/Products';
+import Products from './components/products/Products';
+import ProductDetail from './components/products/ProductDetail';
 
 function App() {
   return (
     <div className="p-4 h-screen w-full">
       <div className="flex flex-row h-full">
-        <NavBar />
-        <div className="flex flex-col w-4/5 border-l p-5">
+        {/* <NavBar /> */}
+        <div className="flex flex-col w-4/5 p-5">
           <Routes>
-            <Route path="/chat" element={<ChatBot />} />
-            <Route path="/products/:id" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/" element={<Products />} />
           </Routes>
         </div>
       </div>
